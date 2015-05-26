961c69f06338d566d6550da3dd12230170ef5fa9
Hiroyuki Sato <sathiroyuki@gmail.com>
2015-05-26 22:27:23 +0900

    プレゼント数の計算をメソッドに抽出

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,9 +1,13 @@
 class BonusDrink
   def self.total_count_for(amount)
+    amount + present_count_for(amount)
+  end
+
+  def self.present_count_for(amount)
     if amount == 0
       0
     else
-      amount + ((amount + 1)/2).floor - 1
+      ((amount + 1)/2).floor - 1
     end
   end
 end
